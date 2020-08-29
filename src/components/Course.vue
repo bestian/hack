c4413f967d73ba160f76e9ae2d0847706abea785
Bestian Tang <bestian@gmail.com>
2020-08-29 08:43:14 +0800

    Update Course.vue

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -72,7 +72,7 @@ export default {
       ],
       students: [
         {n: 'Minecraft介紹網站', h: 'https://github.com/Hans-lee-2006/minecraft/', a: 'Hans-lee-2006'},
-        {n: '自製模型介紹網站', h: 'https://github.com/jinglunT/model', a: 'jinglunT'},
+        {n: '自製模型介紹網站', h: 'https://jinglunt.github.io/model/#/', a: 'jinglunT'},
         {n: 'BMI計算機', h: 'https://github.com/bestian/bmi', a: '共同創作'},
         {n: 'fb釣魚網站習作', h: 'https://github.com/Hans-lee-2006/fb/', a: 'Hans-lee-2006'}
       ]
