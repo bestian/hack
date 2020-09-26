b8472e3ccd3aef87ade61eb4cceafcdd2b199a2d
Bestian Tang <bestian@gmail.com>
2020-09-26 08:50:27 +0800

    Update Course.vue

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -73,9 +73,9 @@ export default {
       students: [
         {n: 'Minecraft介紹網站', h: 'https://hans-lee-2006.github.io/minecraft/#/', a: 'Hans-lee-2006'},
         {n: '自製模型介紹網站', h: 'https://jinglunt.github.io/model/#/', a: 'jinglunT'},
-        {n: 'BMI計算機', h: 'https://github.com/bestian/bmi', a: '共同創作'},
-        {n: 'fb釣魚網站習作', h: 'https://github.com/Hans-lee-2006/fb/', a: 'Hans-lee-2006'},
-        {n: '3D迷宮遊戲', h: 'https://bestian.github.io/maze/', a: '共同創作'}
+        {n: 'BMI計算機', h: 'https:/bestian.github.io/bmi/#/', a: '共同創作'},
+        {n: 'fb釣魚網站習作', h: 'https://hans-lee-2006.github.io/fb/#/', a: 'Hans-lee-2006'},
+        {n: '3D迷宮遊戲', h: 'https://bestian.github.io/maze/#/', a: '共同創作'}
       ]
     }
   }
