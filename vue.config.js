06244d3d76be74743b8709757475b0d0dee0d93d
Bestian Tang 小巴 <bestian@gmail.com>
2020-12-15 19:16:11 +0800

    Update vue.config.js
    test PWA

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  css: { sourceMap: true },
+  productionSourceMap: true,
+  pwa: {
+    workboxOptions: {
+      exclude: [/\.gitkeep/]
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
     : '/'
