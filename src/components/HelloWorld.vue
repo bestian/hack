bb5c5b729d8a2dd421d3731fe8ab019cb7415b42
Bestian Tang 小巴 <bestian@gmail.com>
2021-01-01 10:37:52 +0800

    lighthouse improved

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,14 +3,14 @@
     .ui.container
       sui-segment(raised='').sunflower
         router-link(to="/course")
-          img.logo(src="../assets/logo.png")
+          img.logo(src="../assets/logo.png", alt="vue")
         sui-header(size="large") 歡迎學習寫網站
         p 以一對1~2人的小規模視訊教學，量身打造合適的學習進程。
         p 激發主動創發的自學精神，培養「彷」、「創」、「改」的良好學習習慣。
         p Vue.js是目前最夯的前端框架之一，簡捷、有力、架構清楚。
         p 按
           router-link(to="/course")
-            img.ui.mini.inline.image.haha(src="../assets/logo.png")
+            img.ui.mini.inline.image.haha(src="../assets/logo.png", alt="vue")
           | 可以跳到下一頁喔~
 
 </template>
