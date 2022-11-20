06fd93fc4ffb7ac831bb3ddcf3f84b91d9044e27
Bestian Tang 小巴 <bestian@gmail.com>
2022-11-20 15:02:18 +0800

    translate About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,49 +8,49 @@
         h3
           img.logo(src="/logo.png")
           br
-          | 精準、高效、客製化
+          | {{$t('about_slogan')}}
         .ui.two.stackable.cards
           .ui.card
             .image
               i.time.icon
             .description
-              h3 我的經歷
+              h3 {{$t('my_history')}}
               .ui.bulleted.list
-                .item 前端網站設計7年
-                .item 手機App設計5年
-                .item 自主學習促進會資訊長2年
-                .item 網站設計教師5年
+                .item {{$t('前端網站設計7年')}}
+                .item {{$t('手機App設計5年')}}
+                .item {{$t('自主學習促進會資訊長2年')}}
+                .item {{$t('網站設計教師5年')}}
           .ui.card
             .image
               i.time.icon
             .description
-              h3 我的著作
+              h3 {{$t('my_books')}}
               .ui.bulleted.list
                 a.item(href="https://www.books.com.tw/products/0010759330", target="_blank", rel="noopener noreferrer")
                   i.book.icon
-                  | 普格碼島的法師-歡樂自學寫程式/網路與書出版
+                  | {{$t('普格碼島的法師-歡樂自學寫程式/網路與書出版')}}
                 a.item(href="https://www.books.com.tw/products/0010687205", target="_blank", rel="noopener noreferrer")
                   i.book.icon
-                  | 跟孩子一起玩數學/遠流出版
+                  | {{$t('跟孩子一起玩數學/遠流出版')}}
                 a.item(href="https://www.books.com.tw/products/0010335877", target="_blank", rel="noopener noreferrer")
                   i.book.icon
-                  | 另類教育在台灣/唐山出版
+                  | {{$t('另類教育在台灣/唐山出版')}}
           .ui.card
             .image
               i.globe.icon
             .description
-              h3 擅長領域
+              h3 {{$t('my_domain')}}
               .ui.bulleted.list
-                .item RWD網站設計
-                .item PWA設計
-                .item Hybrid手機App設計
-                .item UI/UX設計
-                .item 前端程式設計
+                .item {{$t('RWD網站設計')}}
+                .item {{$t('PWA設計')}}
+                .item {{$t('Hybrid手機App設計')}}
+                .item {{$t('UI/UX設計')}}
+                .item {{$t('前端程式設計')}}
           .ui.card
             .image
               i.plug.icon
             .description
-              h3 擅長工具
+              h3 {{$t('擅長工具')}}
               .ui.bulleted.list
                 .item html5 + css3
                 .item vue.js
@@ -61,7 +61,7 @@
             .image
               i.mail.icon
             .description
-              h3 聯絡方式
+              h3 {{$t('Connect_Me')}}
               .ui.bulleted.list
                 a.item(href="https://www.tasker.com.tw/workroom/aaRdK2", target="_blank", rel="noopener noreferrer") Tasker ID: bestian
                 a.item(href="mailto:bestian@gmail.com") Email: bestian@gmail.com
@@ -70,13 +70,8 @@
 </template>
 
 <script>
-import Timeline from 'timeline-vuejs';
-
 export default {
   name: 'AbOut',
-  components: {
-    Timeline,
-  },
   metaInfo () { 
     return {
       title: this.$t('about_me')
